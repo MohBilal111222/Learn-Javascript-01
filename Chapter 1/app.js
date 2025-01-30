5ef2dabaacc54b60c0d66d105820bbe4144bff59
Mohammad Bilal <mohammadbilal91112@gmail.com>
2025-01-31 02:15:49 +0500

    learning on it

---
+++ Chapter 1/app.js
@@ -0,0 +1,43 @@
+
+// difference between let const and var
+
+// let b = "abdullah";
+// // console.log(b);
+
+// console.log(b);
+
+
+
+// const author = "bilal";
+// console.log(author);
+
+
+
+
+
+
+
+
+// nn bb u
+
+
+
+// primitives and Objects in javaScript
+
+
+
+
+
+
+// let a = null;
+// let b = 12345;
+// let c = true
+// let d = "Bilal";
+// let e = Symbol ('I am a nice person ');
+// let f = undefined 
+
+
+
+// console.log(a ,b , c ,d ,e ,f)
+
+ 
---
+++ Chapter 2/app.js
@@ -0,0 +1,75 @@
+// q1
+// let a = "bilal";
+// let b = 20;
+
+
+// console.log( a + b)
+
+// Q2
+
+// console.log(typeof (a+b))
+
+
+
+
+
+
+// const bilal = {
+//     name : "bilal"
+    
+// }
+
+//  console.log("bilal")
+
+
+
+ //  question no 3
+
+// const dictionary = {
+//     home: "a lovely home",
+//     about: "more about you and your appreciation",
+//     services: "its totally service based company" 
+//   };
+  
+
+//   console.log(dictionary)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Expressions&Conditions
+
+
+
+
+
+let a = 45;
+let b = 39;
+let c = 98;
+
+
+
+console.log("a + b =")
