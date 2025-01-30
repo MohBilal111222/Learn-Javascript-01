3ea74b29660fbd3c3032eab56910dd637ebb71a4
Mohammad Bilal <mohammadbilal91112@gmail.com>
2025-01-31 03:58:31 +0500

    learning

--- Chapter 2/app.js
+++ Chapter 2/app.js
@@ -60,16 +60,101 @@
 
 
 
-// Expressions&Conditions
+                                 // Operators and Expressions
 
 
 
 
 
-let a = 45;
-let b = 39;
-let c = 98;
+// let a = 45;
+// let b = 39;
+// let c = 98;
 
 
 
-console.log("a + b =")
+// console.log("a + b =")
+
+
+
+
+
+// let a = 50;
+// let b = 6;
+
+
+// console.log("a + b =", a+b);
+
+// console.log("a - b =", a-b);
+//                                             //   divide
+// console.log("a / b =", a/b); 
+//                                             // multiply
+// console.log("a ** b =", a**b);
+//                                             // cuttings even numbers like 2 4 6 8
+// console.log("a % b =", a%b);
+//                                             // pre increment
+// console.log("++a =", ++a);
+//                                             // post increment
+// console.log("a++ =", a++);
+
+// console.log("--a =", --a);
+
+// console.log("a-- =", a--);
+
+// console.log("a =", a);
+
+
+
+
+
+
+
+                                  //ASSIGNMENT OPERATORS
+
+
+
+// let assignment = 1;
+
+
+//  assignment += 5      //same as assignment = assignment + 5
+
+
+//  console.log(assignment);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+                // COMPARISION OPERATORS
+
+
+
+// let comp1 = 8;
+// let comp2 = 9;
+
+
+// console.log(`comp1 == comp2 is ${comp1 == comp2}`);
+
+
+
+
+
+
+
+                 // LOGICAL OPERATORS
+
+let x = 3;
+let y = 9;
+
+console.log(x<y && x==5)
+console.log(x>y || x==5)
+console.log(!false);
+console.log(!true);
